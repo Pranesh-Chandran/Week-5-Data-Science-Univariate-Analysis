e4636301e5652eb21c6b4e0faebfcd37c5b60a6d
Pranesh-Chandran <praneshkumar8@gmail.com>
2026-05-04 11:00:52 +0530

    Add files via upload

---
+++ 6. Data-Preprocessing/Univariate.py
@@ -0,0 +1,33 @@
+class Univariate():
+    def quanQual(dataset):
+        quan=[]
+        qual=[]
+        for columnName in dataset.columns:
+            if(dataset[columnName].dtype == "O"):
+                qual.append(columnName)
+            else:
+                quan.append(columnName)
+        return quan,qual
+
+    def Univariate(dataset,quan):
+        descriptive = pd.DataFrame(index=["Mean","Median","Mode","25%","50%","75%","100%","IQR","1.5*IQR","Lesser","Greater","Min",
+                                          "Max","Skewness","Kurtosis","Variance","Std"],columns=quan)
+        for columnName in quan:
+            descriptive[columnName]["Mean"] = dataset[columnName].mean()
+            descriptive[columnName]["Median"] = dataset[columnName].median()
+            descriptive[columnName]["Mode"] = dataset[columnName].mode()[0]
+            descriptive[columnName]["25%"] = dataset.describe()[columnName]["25%"]
+            descriptive[columnName]["50%"] = dataset.describe()[columnName]["50%"]
+            descriptive[columnName]["75%"] = dataset.describe()[columnName]["75%"]
+            descriptive[columnName]["100%"] = dataset.describe()[columnName]["max"]
+            descriptive[columnName]["IQR"] = descriptive[columnName]["75%"] - descriptive[columnName]["25%"]
+            descriptive[columnName]["1.5*IQR"] = 1.5 * descriptive[columnName]["IQR"]
+            descriptive[columnName]["Lesser"] = descriptive[columnName]["25%"] - descriptive[columnName]["1.5*IQR"]
+            descriptive[columnName]["Greater"] = descriptive[columnName]["75%"] + descriptive[columnName]["1.5*IQR"]
+            descriptive[columnName]["Min"] = dataset[columnName].min()
+            descriptive[columnName]["Max"] = dataset[columnName].max()
+            descriptive[columnName]["Skewness"] = dataset[columnName].skew()
+            descriptive[columnName]["Kurtosis"] = dataset[columnName].kurtosis()
+            descriptive[columnName]["Variance"] = dataset[columnName].var()
+            descriptive[columnName]["Std"] = dataset[columnName].std()
+        return descriptive
